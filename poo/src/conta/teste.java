6fe2b4366bb3bac66ea5fe15b5e1809f88c31f8a
achilles_pc <achilles_pc@DESKTOP-IT008TB>
2020-03-05 10:51:34 -0300

    adicionado a biblioteca scanner

--- poo/src/conta/teste.java
+++ poo/src/conta/teste.java
@@ -1,23 +1,39 @@
 package conta;
 
+import java.util.Scanner;
+
 //import java.awt.Container;
 
 public class teste {
-	
-	
+
 	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);// Biblioteca para poder adicionar valores digitados
+
+		conta contac = new conta();
+
+		System.out.println("Ola digite o nome do titular da conta");
+
+		contac.nome = sc.nextLine(); // adiciona o nome a conta
 		
-      conta contac= new conta();
-      
-      contac.nome= "achilles";
-      contac.numero=1209;
-      contac.saldo =1000;
-      
-      
-      contac.depositar(500);
-      contac.sacar(300);
-      System.out.println(contac.saldo);
-      
-}
+		
+		
+		System.out.println("Ola digite o numero da conta");
+
+		contac.numero = sc.nextInt(); // adiciona o numero da conta
+		
+		
+		System.out.println("Ola digite o sado inicial ");
+		contac.saldo = sc.nextDouble(); // adiciona o saldo 
+		
+		System.out.println("Ola digite quanto você que depositar");
+		
+		contac.depositar(sc.nextDouble()); // vamos fazer um deposito
+		
+		
+		
+		contac.sacar(300); //um saque
+		System.out.println(contac.saldo);
+
+	}
 
 }
