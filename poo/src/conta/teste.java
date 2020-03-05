c78cd4f01f9cb7541f0b1fb46ef7a735348630e1
achilles_pc <achilles_pc@DESKTOP-IT008TB>
2020-03-05 10:42:01 -0300

    Criando o teste de saque

--- poo/src/conta/teste.java
+++ poo/src/conta/teste.java
@@ -10,12 +10,14 @@ public class teste {
       conta contac= new conta();
       
       contac.nome= "achilles";
-
-
-
-
-		
-		
-	}
+      contac.numero=1209;
+      contac.saldo =1000;
+      
+      
+      contac.depositar(500);
+      contac.sacar(300);
+      System.out.println(contac.saldo);
+      
+}
 
 }
